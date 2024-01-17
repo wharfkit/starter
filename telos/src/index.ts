0b608442bf433369697fd447aa063b7e4fe248c7
Aaron Cox <aaron@greymass.com>
2024-01-16 16:19:31 -0800

    Adding Wombat to telos

--- telos/src/index.ts
+++ telos/src/index.ts
@@ -1,38 +1,40 @@
-export * from "@wharfkit/starter"
+export * from '@wharfkit/starter'
 
 import {
-  // AccountKit,
-  APIClient,
-  // Chains,
-  ContractKit,
-  SessionKit,
-  TransactPluginResourceProvider,
-  WalletPluginAnchor,
-  WebRenderer
-} from "@wharfkit/starter"
+    // AccountKit,
+    APIClient,
+    // Chains,
+    ContractKit,
+    SessionKit,
+    TransactPluginResourceProvider,
+    WalletPluginAnchor,
+    WebRenderer,
+} from '@wharfkit/starter'
+import {WalletPluginWombat} from '@wharfkit/wallet-plugin-wombat'
 
-const chainUrl = "https://telos.greymass.com"
+const chainUrl = 'https://telos.greymass.com'
 
-export const sessionKit = new SessionKit({
-  appName: "WharfKit App",
-  chains: [
+export const sessionKit = new SessionKit(
     {
-      id: "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11",
-      url: chainUrl,
+        appName: 'WharfKit App',
+        chains: [
+            {
+                id: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
+                url: chainUrl,
+            },
+        ],
+        ui: new WebRenderer(),
+        walletPlugins: [new WalletPluginAnchor(), new WalletPluginWombat()],
     },
-  ],
-  ui: new WebRenderer(),
-  walletPlugins: [new WalletPluginAnchor()],
-}, {
-  transactPlugins: [new TransactPluginResourceProvider()],
-})
+    {
+        transactPlugins: [new TransactPluginResourceProvider()],
+    }
+)
 
 // Uncomment once we can get the Account Kit to work with rush monorepos
 // see https://github.com/microsoft/TypeScript/issues/42873
 // export const accountKit = new AccountKit(Chains.Telos)
 
 export const contractKit = new ContractKit({
-  client: new APIClient({ url: chainUrl })
+    client: new APIClient({url: chainUrl}),
 })
-
-
