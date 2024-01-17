7d17a93f291fd715b46f687432f847b4460cf268
Aaron Cox <aaron@greymass.com>
2024-01-16 16:05:19 -0800

    Added TokenPocket to config

--- eos/src/index.ts
+++ eos/src/index.ts
@@ -1,40 +1,45 @@
-export * from "@wharfkit/starter"
+export * from '@wharfkit/starter'
 
 import {
-  // AccountKit,
-  APIClient,
-  // Chains,
-  ContractKit,
-  SessionKit,
-  TransactPluginResourceProvider,
-  WalletPluginAnchor,
-  WebRenderer
-} from "@wharfkit/starter"
-import { WalletPluginWombat } from "@wharfkit/wallet-plugin-wombat" // Uncomment once the Wombat plugin is updated to the latest version of the Session Kit
+    // AccountKit,
+    APIClient,
+    // Chains,
+    ContractKit,
+    SessionKit,
+    TransactPluginResourceProvider,
+    WalletPluginAnchor,
+    WebRenderer,
+} from '@wharfkit/starter'
+import {WalletPluginWombat} from '@wharfkit/wallet-plugin-wombat'
+import {WalletPluginTokenPocket} from '@wharfkit/wallet-plugin-tokenpocket'
 
-const chainUrl = "https://eos.greymass.com"
+const chainUrl = 'https://eos.greymass.com'
 
-export const sessionKit = new SessionKit({
-  appName: "WharfKit App",
-  chains: [
+export const sessionKit = new SessionKit(
     {
-      id: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-      url: chainUrl,
+        appName: 'WharfKit App',
+        chains: [
+            {
+                id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+                url: chainUrl,
+            },
+        ],
+        ui: new WebRenderer(),
+        walletPlugins: [
+            new WalletPluginAnchor(),
+            new WalletPluginTokenPocket(),
+            new WalletPluginWombat(),
+        ],
     },
-  ],
-  ui: new WebRenderer(),
-  walletPlugins: [
-    new WalletPluginAnchor(),
-    new WalletPluginWombat(),
-  ],
-}, {
-  transactPlugins: [new TransactPluginResourceProvider()],
-})
+    {
+        transactPlugins: [new TransactPluginResourceProvider()],
+    }
+)
 
 // Uncomment once we can get the Account Kit to work with rush monorepos
 // see https://github.com/microsoft/TypeScript/issues/42873
-// export const accountKit = new AccountKit(Chains.EOS) 
+// export const accountKit = new AccountKit(Chains.EOS)
 
 export const contractKit = new ContractKit({
-  client: new APIClient({ url: chainUrl })
+    client: new APIClient({url: chainUrl}),
 })
