58d0b508d36f89d530ea8327232daa901d6c7c62
Aaron Cox <aaron@greymass.com>
2024-01-16 16:25:34 -0800

    Add Wombat to WAX

--- wax/src/index.ts
+++ wax/src/index.ts
@@ -1,40 +1,45 @@
-export * from "@wharfkit/starter"
+export * from '@wharfkit/starter'
 
 import {
-  // AccountKit,
-  APIClient,
-  // Chains,
-  ContractKit,
-  SessionKit,
-  TransactPluginResourceProvider,
-  WalletPluginAnchor,
-  WebRenderer
-} from "@wharfkit/starter"
-import { WalletPluginCloudWallet } from "@wharfkit/wallet-plugin-cloudwallet"
+    // AccountKit,
+    APIClient,
+    // Chains,
+    ContractKit,
+    SessionKit,
+    TransactPluginResourceProvider,
+    WalletPluginAnchor,
+    WebRenderer,
+} from '@wharfkit/starter'
+import {WalletPluginCloudWallet} from '@wharfkit/wallet-plugin-cloudwallet'
+import {WalletPluginWombat} from '@wharfkit/wallet-plugin-wombat'
 
-const chainUrl = "https://wax.greymass.com"
+const chainUrl = 'https://wax.greymass.com'
 
-export const sessionKit = new SessionKit({
-  appName: "WharfKit App",
-  chains: [
+export const sessionKit = new SessionKit(
     {
-      id: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
-      url: chainUrl,
+        appName: 'WharfKit App',
+        chains: [
+            {
+                id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
+                url: chainUrl,
+            },
+        ],
+        ui: new WebRenderer(),
+        walletPlugins: [
+            new WalletPluginAnchor(),
+            new WalletPluginCloudWallet(),
+            new WalletPluginWombat(),
+        ],
     },
-  ],
-  ui: new WebRenderer(),
-  walletPlugins: [
-    new WalletPluginAnchor(),
-    new WalletPluginCloudWallet()
-  ],
-}, {
-  transactPlugins: [new TransactPluginResourceProvider()],
-})
+    {
+        transactPlugins: [new TransactPluginResourceProvider()],
+    }
+)
 
 // Uncomment once we can get the Account Kit to work with rush monorepos
 // see https://github.com/microsoft/TypeScript/issues/42873
 // export const accountKit = new AccountKit(Chains.WAX)
 
 export const contractKit = new ContractKit({
-  client: new APIClient({ url: chainUrl })
+    client: new APIClient({url: chainUrl}),
 })
